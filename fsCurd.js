1e526db4b7b093d81add7bcaf5df10afcfb18e35
mr-sandip-das <sandipdas.0106@gmail.com>
2026-09-23 23:03:36 +0530

    croud with file system

---
+++ fsCurd.js
@@ -0,0 +1,15 @@
+const fs=require("fs");
+const operation=process.argv[2];
+if (operation=="write"){
+    const name=process.argv[3];
+    const content=process.argv[4];
+    console.log();
+    
+}
+
+// fs.writeFileSync("files/appale.txt","this is apple file");
+// fs.writeFileSync("files/hii.html","<h1>hello sandip</h1>");
+// fs.unlinkSync("files/hii.html");
+// let data=fs.readFileSync("files/appale.txt","utf-8");
+// console.log(data);
+
