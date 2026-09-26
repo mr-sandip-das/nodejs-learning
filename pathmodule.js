893146a008f06437d76f5aed5feb96fb45bb92c8
mr-sandip-das <sandipdas.0106@gmail.com>
2026-09-26 23:27:24 +0530

    path module and global constant

---
+++ pathmodule.js
@@ -0,0 +1,9 @@
+const path=require("path");
+const file="sandip/text/check.txt";
+// console.log(path.extname(file));
+// console.log(path.dirname(file));
+// console.log(path.basename(file));
+// console.log(path.resolve("sandip/text","check.txt"));
+// console.log(path.isAbsolute(file));
+console.log(__dirname);
+console.log(__filename);
